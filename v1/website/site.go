11214359d1ac8890342f5179805f9cfb3b41c2e9
Brian W. Wolter <brianwolter@gmail.com>
2024-05-19 14:54:14 +0200

    Init

---
+++ v1/website/site.go
@@ -0,0 +1,70 @@
+package website
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+var errCouldNotResolve = errors.New("Could not resolve identity")
+
+var client = &http.Client{}
+
+type Info struct {
+	Owner    string // The name of the owner of the site, e.g., a company name, as best we can determine
+	Homepage string // The homepage of the site
+}
+
+// Attempt to infer details about a website from its domain name
+func IdentifyDomain(cxt context.Context, domain string) (Info, error) {
+	return IdentifyWebsite(cxt, fmt.Sprintf("https://%s", domain))
+}
+
+// Attempt to infer details about a website
+func IdentifyWebsite(cxt context.Context, link string) (Info, error) {
+	req, err := http.NewRequestWithContext(cxt, "GET", link, nil)
+	if err != nil {
+		return Info{}, nil
+	}
+
+	rsp, err := client.Do(req)
+	if err != nil {
+		return Info{}, fmt.Errorf("Could not fech website: %w", err)
+	}
+	if rsp.StatusCode != http.StatusOK {
+		return Info{}, fmt.Errorf("Unexpected response status: %s", rsp.Status)
+	}
+	if rsp.Body == nil {
+		return Info{}, fmt.Errorf("No content returned")
+	}
+	defer rsp.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(rsp.Body)
+	if err != nil {
+		return Info{}, fmt.Errorf("Could not process document: %w", err)
+	}
+
+	return identifyWebsite(cxt, link, doc)
+}
+
+func identifyWebsite(cxt context.Context, link string, doc *goquery.Document) (Info, error) {
+	var sel *goquery.Selection
+	var info Info
+
+	if sel = doc.Find(`head link[rel="canonical"]`); sel.Length() > 0 {
+		info.Homepage = sel.First().AttrOr("href", link)
+	} else if sel = doc.Find(`head meta[property="og:url"]`); sel.Length() > 0 {
+		info.Homepage = sel.First().AttrOr("content", link)
+	} else {
+		info.Homepage = link
+	}
+
+	if sel = doc.Find(`head meta[property="og:site_name"]`); sel.Length() > 0 {
+		info.Owner = sel.First().AttrOr("content", "")
+	}
+
+	return info, nil
+}
---
+++ v1/website/site_test.go
@@ -0,0 +1,80 @@
+package website
+
+import (
+	"bytes"
+	"context"
+	"testing"
+
+	"github.com/PuerkitoBio/goquery"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIdentifyWebsite(t *testing.T) {
+	tests := []struct {
+		Input  string
+		Expect Info
+		Error  error
+	}{
+		{
+			Input: `<!DOCTYPE html>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+		<meta name='robots' content='index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1' />
+		<!-- This site is optimized with the Yoast SEO plugin v22.7 - https://yoast.com/wordpress/plugins/seo/ -->
+		<title>Home - EverHealth</title>
+		<meta property="og:locale" content="en_US" />
+		<meta property="og:type" content="website" />
+		<meta property="og:title" content="Home - EverHealth" />
+		<meta property="og:description" content="Reimagining the Way You Work Our simplified, user-centric software can streamline daily operations, connect you with patients, and keep your practice thriving. Stay in the Know Simplifying the Business of Healthcare.SM We’ve listened to feedback from practice managers, physicians, and other healthcare professionals. We understand how critical the right software can be to the health [&hellip;]" />
+		<meta property="og:url" content="https://www.everhealth.com/meta" />
+		<meta property="og:site_name" content="EverHealth" />
+		<meta property="article:modified_time" content="2024-05-14T19:13:39+00:00" />
+		<meta property="og:image" content="https://www.everhealth.com/wp-content/uploads/everhealth-logo.svg" />
+		<meta name="twitter:card" content="summary_large_image" />
+	</head>
+</html>`,
+			Expect: Info{
+				Owner:    "EverHealth",
+				Homepage: "https://www.everhealth.com/meta",
+			},
+		},
+		{
+			Input: `<!DOCTYPE html>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+		<meta name='robots' content='index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1' />
+		<!-- This site is optimized with the Yoast SEO plugin v22.7 - https://yoast.com/wordpress/plugins/seo/ -->
+		<title>Home - EverHealth</title>
+		<link rel="canonical" href="https://www.everhealth.com/link" />
+		<meta property="og:locale" content="en_US" />
+		<meta property="og:type" content="website" />
+		<meta property="og:title" content="Home - EverHealth" />
+		<meta property="og:description" content="Reimagining the Way You Work Our simplified, user-centric software can streamline daily operations, connect you with patients, and keep your practice thriving. Stay in the Know Simplifying the Business of Healthcare.SM We’ve listened to feedback from practice managers, physicians, and other healthcare professionals. We understand how critical the right software can be to the health [&hellip;]" />
+		<meta property="og:url" content="https://www.everhealth.com/meta" />
+		<meta property="og:site_name" content="EverHealth" />
+		<meta property="article:modified_time" content="2024-05-14T19:13:39+00:00" />
+		<meta property="og:image" content="https://www.everhealth.com/wp-content/uploads/everhealth-logo.svg" />
+		<meta name="twitter:card" content="summary_large_image" />
+	</head>
+</html>`,
+			Expect: Info{
+				Owner:    "EverHealth",
+				Homepage: "https://www.everhealth.com/link",
+			},
+		},
+	}
+	for _, e := range tests {
+		doc, err := goquery.NewDocumentFromReader(bytes.NewBuffer([]byte(e.Input)))
+		if !assert.NoError(t, err) {
+			continue
+		}
+		info, err := identifyWebsite(context.TODO(), "https://default.com", doc)
+		if e.Error != nil {
+			assert.ErrorIs(t, err, e.Error)
+		} else if assert.NoError(t, err) {
+			assert.Equal(t, e.Expect, info)
+		}
+	}
+}
